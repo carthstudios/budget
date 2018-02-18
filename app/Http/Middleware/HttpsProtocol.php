b1a2541c068b5933199ea9cd1edefa26192f89f4
carth <carthstudios@gmail.com>
2018-02-18 23:03:14 +0100

    fix force https

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -2,13 +2,16 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Log;
 
 class HttpsProtocol
 {
     public function handle($request, Closure $next)
     {
         if (!$request->secure() && env('APP_ENV') === 'prod') {
-            return redirect()->secure($request->getRequestUri());
+            $url = url()->current();
+            $url = str_replace_first("http://", "https://", $url);
+            return redirect()->secure($url);
         }
 
         return $next($request);
